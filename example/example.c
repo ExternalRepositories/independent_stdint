e3787fde5902b066eaad4abe0950e2c3700a5db2
Anton Kesy <anton@kesy.de>
2021-10-03 10:51:57 +0200

    create simple usage examples

---
+++ example/example.c
@@ -0,0 +1,14 @@
+#include "../independent_int.h"
+
+int main(void)
+{
+    uint8_t u_number = 42;
+    iuint_8_t iu_number = fill_iuint_8(&u_number);
+    //write/read iuint to file
+    uint8_t u_number_copy = get_iuint_8_value(&iu_number);
+    if (u_number != u_number_copy)
+    {
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
+}
